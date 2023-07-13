c5285175934f5795f28dd00bc87aa7095ccd371e
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-13 14:05:56 +0300

    Create quicksort.py

---
+++ Searching and Sorting Algorithms - Lab/quicksort.py
@@ -0,0 +1,24 @@
+def qsort(start, end, nums):
+    if start >= end:
+        return
+
+    pivot, left, right = start, start + 1, end
+
+    while left <= right:
+        if nums[left] <= nums[pivot]:
+            left += 1
+        elif nums[right] >= nums[pivot]:
+            right -= 1
+        else:
+            nums[left], nums[right] = nums[right], nums[left]
+
+    nums[pivot], nums[right] = nums[right], nums[pivot]
+
+    qsort(start, right - 1, nums)
+    qsort(right + 1, end, nums)
+
+
+nums = [int(x) for x in input().split()]
+
+qsort(0, len(nums) - 1, nums)
+print(*nums, sep=" ")
