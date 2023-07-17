3aa8552db58cb1273af4b501566eac9a62fc05d9
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-17 21:00:18 +0300

    Create topological_sorting_with_bfs.py

---
+++ Graph Theory, Traversal and Topological Sorting - Lab/topological_sorting_with_bfs.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+
+nodes = int(input())
+graph = {}
+
+for _ in range(nodes):
+    line = input().split("->")
+    node = line[0].strip()
+    children = line[1].strip().split(", ") if line[1].strip() else []
+    graph[node] = children
+
+visited = set()
+cycles = set()
+
+
+def dfs(node, graph, visited, cycles, sorted_nodes):
+    if node in cycles:
+        raise Exception("cycle detected")
+
+    if node in visited:
+        return
+
+    visited.add(node)
+    cycles.add(node)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, cycles, sorted_nodes)
+
+    cycles.remove(node)
+    sorted_nodes.appendleft(node)
+
+
+sorted_nodes = deque()
+for node in graph:
+    dfs(node, graph, visited, cycles, sorted_nodes)
+
+print(*sorted_nodes, sep=" ")
+
+
+
+
