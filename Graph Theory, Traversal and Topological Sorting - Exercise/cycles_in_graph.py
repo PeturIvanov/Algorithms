6135b7a16854fd32aa91c381275a16cfdebdb81f
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-18 15:05:06 +0300

    Update cycles_in_graph.py

--- Graph Theory, Traversal and Topological Sorting - Exercise/cycles_in_graph.py
+++ Graph Theory, Traversal and Topological Sorting - Exercise/cycles_in_graph.py
@@ -1,50 +1,3 @@
-graph = {}
-while True:
-    input_line = input()
-    if input_line == "End":
-        break
-
-    parent, child = input_line.split("-")
-
-    if parent not in graph:
-        graph[parent] = []
-
-    graph[parent].append(child)
-
-is_cyclic = True
-visited = []
-for node, children in graph.items():
-    visited.append(node)
-
-    for child in children:
-        if child in visited:
-            is_cyclic = False
-
-if is_cyclic:
-    print("Acyclic: Yes")
-
-else:
-    print("Acyclic: No")
-
-
-
-# Second solution with dfs:
-
-# def dfs(node, graph, visited, cycle):
-#     if node in cycle:
-#         print("Acyclic: No")
-#         exit()
-#
-#     if node in visited:
-#         return
-#
-#     visited.add(node)
-#     cycle.add(node)
-#
-#     for child in graph[node]:
-#         dfs(child, graph, visited, cycle)
-#
-#     cycle.remove(node)
 # graph = {}
 # while True:
 #     input_line = input()
@@ -58,13 +11,57 @@ else:
 #
 #     graph[parent].append(child)
 #
-#     if child not in graph:
-#         graph[child] = []
+# is_cyclic = True
+# visited = []
+# for node, children in graph.items():
+#     visited.append(node)
 #
-# visited = set()
-# cycle = set()
+#     for child in children:
+#         if child in visited:
+#             is_cyclic = False
+#
+# if is_cyclic:
+#     print("Acyclic: Yes")
+#
+# else:
+#     print("Acyclic: No")
 
-# for node in graph:
-#     dfs(node, graph, visited, cycle)
-#
-# print("Acyclic: Yes")
+
+def dfs(node, graph, visited, cycle):
+    if node in cycle:
+        print("Acyclic: No")
+        exit()
+
+    if node in visited:
+        return
+
+    visited.add(node)
+    cycle.add(node)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, cycle)
+
+    cycle.remove(node)
+graph = {}
+while True:
+    input_line = input()
+    if input_line == "End":
+        break
+
+    parent, child = input_line.split("-")
+
+    if parent not in graph:
+        graph[parent] = []
+
+    graph[parent].append(child)
+
+    if child not in graph:
+        graph[child] = []
+
+visited = set()
+cycle = set()
+
+for node in graph:
+    dfs(node, graph, visited, cycle)
+
+print("Acyclic: Yes")
